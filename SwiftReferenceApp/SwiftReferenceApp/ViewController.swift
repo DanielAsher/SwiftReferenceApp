b344cba89ca9c748d8795490599ee1f36052aade
Daniel Asher <daniel.asher.2303@gmail.com>
2015-08-21 10:37:28 +0100

    Add SwiftReferenceApp Xcode project

---
+++ SwiftReferenceApp/SwiftReferenceApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftReferenceApp
+//
+//  Created by Daniel Asher on 21/08/2015.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
