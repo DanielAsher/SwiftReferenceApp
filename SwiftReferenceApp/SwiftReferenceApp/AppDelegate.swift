2333ac93c77b097c9f3da6b2a5fd1b5f577c3c5e
Daniel Asher <daniel.asher.2303@gmail.com>
2015-08-21 18:50:20 +0100

    create User and .Start App in AppDelegate.didFinishLaunchingWithOptions

--- SwiftReferenceApp/SwiftReferenceApp/AppDelegate.swift
+++ SwiftReferenceApp/SwiftReferenceApp/AppDelegate.swift
@@ -13,13 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     let app = App.sharedInstance
-
+    var currentUser : User!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        currentUser = User()
         app.handleEvent(.Start)
-        
         return true
     }
 
