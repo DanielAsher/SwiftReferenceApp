6fb94803fd79c60af91ae2882158a83912810f2b
Daniel Asher <daniel.asher.2303@gmail.com>
2015-08-26 19:54:46 +0100

    add SimpleAsyncSpec

---
+++ SwiftReferenceApp/SwiftReferenceAppTests/SimpleAsyncSpec.swift
@@ -0,0 +1,53 @@
+//
+//  SimpleAsyncSpec.swift
+//  SwiftReferenceApp
+//
+//  Created by Daniel Asher on 26/08/2015.
+//  Copyright (c) 2015 StoryShare. All rights reserved.
+//
+
+import Nimble
+import Quick
+import RxSwift
+
+class AsyncSpec: QuickSpec 
+{
+    override func spec() 
+    {
+        describe("async") {
+            
+            let machineState = Variable("Initial")
+            
+            machineState >- debug("machineState:") >- subscribeNext { _ in return }
+            
+            let createService = { (resultState: String) -> Disposable in
+                
+                machineState.next("Awaiting")
+                
+                return interval(3.0, MainScheduler.sharedInstance) 
+                    >- take(1) // take only the next async `result`. 
+                    // Here we ignore that and simply set to `modified`.
+                    >- debug("service:")                 
+                    >- subscribeNext { result in machineState.next(resultState) } 
+            }
+            
+            beforeSuite {
+                
+            }
+            
+            beforeEach { createService("Modified") }
+            
+            afterEach { machineState.next("Idle") }
+            
+            it("should do some async operation") {
+                
+                expect(machineState.value).toEventually(beginWith("Modified"), timeout: 5)                
+                
+                createService("LocallyModified")
+                
+                expect(machineState.value).toEventually(beginWith("LocallyModified"), timeout: 5) 
+            }
+        }
+    }
+}
+
