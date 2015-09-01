e1a3108dfe23ba8d2e4090373678c0dc5f27d717
Daniel Asher <daniel.asher.2303@gmail.com>
2015-09-01 09:14:16 +0100

    complete first working implementation of DOT Language parser. still need to support quoted strings, floating-point numbers and `{rank=same 0 0}`

--- SwiftReferenceApp/DotParser.playground/Contents.swift
+++ SwiftReferenceApp/DotParser.playground/Contents.swift
@@ -7,12 +7,12 @@ An abstract syntax tree and parser for [The DOT Language](http://www.graphviz.or
 To enable generation of the follow type of diagram.
 ![StateMachine](ApplicationSchema.png)
 */
-//: Syntax Tree
+//: Imports
 import Prelude
 import Either
 import Madness
 import RxSwift
-
+//: Syntax Tree Types
 typealias ID = String
 
 struct Attribute {
@@ -38,7 +38,6 @@ struct EdgeRHS {
 
 enum Statement {
     case Node(id: ID, attributes: [Attribute])
-//: FIXME: `Edge` doesn't support `Subgraph` source and target
     case Edge(source: ID, edgeRHS: [EdgeRHS], attributes: [Attribute])
     case Attr(target: TargetType, attributes: [Attribute])
     case Property(Attribute)
@@ -83,18 +82,28 @@ extension EdgeRHS : Printable {
 extension Statement : Printable {
     var description : String {
         switch self {
-            case Node(let id, let xs): return "Node ( \(id), \(xs) )"
-            case Edge(let src, let es, let xs): return "Edge ( \(src), \(es), \(xs)"
-            case Attr(let tgt, let xs): return "Attr ( \(tgt), \(xs) )"
-            case Property(let attribute): return "Property ( \(attribute) )"
-            case Subgraph(let id, let stmts): return "Subgraph ( \(id), \(stmts) )"
+            case Node(let id, let xs): return "Node ( \(id), \(xs) )\n\t"
+            case Edge(let src, let es, let xs): return "Edge ( \(src), \(es), \(xs) )\n\t"
+            case Attr(let tgt, let xs): return "Attr ( \(tgt), \(xs) )\n\t"
+            case Property(let attribute): return "Property ( \(attribute) )\n\t"
+            case Subgraph(let id, let stmts):
+                return "Subgraph ( id: \"" + (id ?? "") + "\", stmts: \(stmts))"
         }
+    }
+}
+
+typealias StatementsParser = Parser<String, [Statement]>.Function
+
+extension GraphType : Printable {
+    var description : String {
+        return self.rawValue
     }
 }
 
 extension Graph : Printable {
     var description : String {
-        return "Graph ( \(self.type), \(self.id), \(self.stmt_list) )"
+        let idstr = self.id ?? ""
+        return "Graph ( type: \"\(self.type)\", id: \"\(idstr)\", stmt_list: \n\t\(self.stmt_list) )"
     }
 }
 
@@ -122,6 +131,7 @@ let arrow        = token ( %"->" )
 let link         = token ( %"--" )
 let semicolon    = token ( %";" )
 let comma        = token ( %"," )
+let quote        = token ( %"\"" )
 
 let separator = token (%";" | %",") 
 let sep       = separator|? |> map { $0 ?? "" }
@@ -136,7 +146,9 @@ let digit   = %("0"..."9")
 2. a numeral [-]?(.[0-9]+ | [0-9]+(.[0-9]*)? )
 3. any double-quoted string ("...") possibly containing escaped quotes ('")1
 4. an HTML string (<...>).
-> FIXME: This only partially implement case (1). Complete cases (2), (3) and (4).
+> FIXME: This only partially implement case (1). Complete cases (2), (3) and (4)!
+> The parser currently REJECTS any non-`id` strings in quotations and floating-point numbers!
+> FIXME: Also rejects `{rank=same 0 0}` !
 */
 let id = (lower | upper | digit | %"_")+
     |> map { "".join($0) }
@@ -144,14 +156,15 @@ let id = (lower | upper | digit | %"_")+
 /*:
 ## _id_stmt_ : ID '=' ID
 */
-let id_equality = id ++ ignore(equal) ++ id ++ ignore(sep)
-    |> map { Attribute(name: $0, value: $1) }
+let id_equality = id ++ ignore(equal) ++ ignore(quote|?) ++ id|? ++ ignore(quote|?)
+    |> map { Attribute(name: $0, value: $1 ?? "") }
 /*:
 ## _a_list_ : id_stmt [ (';' | ',') ] [ _a_list_ ]
 */
 let a_list = 
     fix { a_list in
-        return id_equality*
+        return id_equality ++ ignore( sep|? ) ++ a_list* 
+            |> map { [$0] + $1.flatMap { $0 } }
     }
 /*: 
 ## _attr_list_ : '[' [ a_list ] ']' [ _attr_list_ ]
@@ -185,10 +198,8 @@ let node_stmt = node_id ++ attr_list*
 */
 let edgeRHS : Parser<String, [EdgeRHS]>.Function = fix { edgeRHS in
     // TODO: Get rid of nested tuples and the maps they require to unfold them.
-    let edgeSpec = edgeop ++ node_id    
-        |> map { [EdgeRHS(edgeOp: $0, target: $1)] }
-    return edgeSpec ++ edgeRHS*         
-        |> map { $0 + $1.flatMap { $0 } }
+    let edgeSpec = edgeop ++ node_id    |> map { [EdgeRHS(edgeOp: $0, target: $1)] }
+    return edgeSpec ++ edgeRHS*         |> map { $0 + $1.flatMap { $0 } }
     }
 /*: 
 ## _edge_stmt_ : (node_id | subgraph) edgeRHS [ attr_list ]
@@ -203,28 +214,27 @@ let edge_stmt = node_id ++ edgeRHS ++ opt_attr
 * _subgraph_    :       [ "subgraph" [ ID ] ] '{' stmt_list '}'
 > FIXME: Yikes! needs mutual recursion. Test! Also Render needs work.
 */
-let stmt_list : Parser<String, [Statement]>.Function = 
-    fix { stmt_list in
+let stmt_list : StatementsParser = fix { stmt_list in
 
-        let id_stmt = id_equality |> map { Statement.Property($0) }
-        
-        let subgraph_id = token (%("subgraph")) ++ id|? |> map { $1 }
+    let id_stmt = id_equality |> map { Statement.Property($0) }
+    
+    let subgraph_id = token (%("subgraph")) ++ id|? |> map { $1 }
 
-        let subgraph = 
-            subgraph_id ++ ignore(leftBrace) ++ stmt_list ++ ignore(rightBrace) 
-            |> map { Statement.Subgraph(id: $0, stmts: $1) }
-            
-        let stmt = node_stmt | edge_stmt | attr_stmt | id_stmt | subgraph
+    let subgraph = subgraph_id ++ ignore(leftBrace) ++ stmt_list ++ ignore(rightBrace) 
+        |> map { Statement.Subgraph(id: $0, stmts: $1) }
+    
+    // TODO: Figure out if the ordering of alternatives is essential
+    let stmt = id_stmt | edge_stmt | attr_stmt | subgraph | node_stmt 
 
-        return stmt ++ ignore(semicolon) ++ stmt_list*
-            |> map { x, xs in [x] + xs.flatMap { $0 } }
-        }
+    return stmt ++ ignore( semicolon|? ) ++ stmt_list*
+        |> map { x, xs in [x] + xs.flatMap { $0 } }
+    }
 /*:
 ## _graph_ : [ "strict" ] ("graph" | "digraph") [ ID ] '{' stmt_list '}'
 We can now define the root of our grammar, **graph**
 */
-//let strict = token ( (%("strict"))|? )
-//
+//: FIXME: `let strict = token ( (%("strict"))|? )`
+
 let graph_type = 
      ( %("graph") | %("digraph") ) 
         |> token
@@ -237,6 +247,12 @@ let graph = graph_id ++ ignore(leftBrace) ++ stmt_list ++ ignore(rightBrace)
 /*:
 ## DotParser Tests
 */
+
+
+let output = parse(graph, applicationSchema)
+println(output.result)
+
+
 let input1 = "compound = true; fontcolor=coral3, a=b \n \t\t hello = world "
 let output1 = parse(a_list, input1).result
 output1 == "[compound = true, fontcolor = coral3, a = b, hello = world]"
@@ -260,13 +276,6 @@ output5 == "[-> ReceiveNode, -> NextNode]"
 let input6 = "SourceState -> TargetState [label = Trigger]"
 let output6 = parse(edge_stmt, input6).result
 output6 == "Edge ( SourceState, [-> TargetState], [label = Trigger]"
-//let applicationSchemaString = "digraph G \n{\n    compound=true\n    graph [rankdir=LR]\n    subgraph cluster0 \n    {\n\n        label=Application\n        color=slateblue2\n        fontcolor=slateblue4\n        margin=30\n        labeljust=l\n\n        appStart [label="", style=invis]\n        appStart -> 1 [label=\"Start\", weight=2, color=darkgreen, fontcolor=darkgreen]\n\n        0 [label=\"Application Start\", color=coral3, fontcolor=coral4]\n        0 -> 1 [xlabel=\"Start\", lhead=cluster0, color=darkgreen, fontcolor=darkgreen]\n\n        1 [label=\"Idle\", color=coral3, fontcolor=coral4]\n        2 [label=\"Saving\", color=turquoise3, fontcolor=turquoise4]\n        3 [label=\"Purchasing\", color=turquoise3, fontcolor=turquoise4]\n        4 [label=\"Alerting\", color=turquoise3, fontcolor=turquoise4]\n\n        1 -> 3 [label=\"Purchase\", color=blue, fontcolor=blue3]\n        1 -> 2 [label=\"Save\", color=blue, fontcolor=blue3]\n\n        2 -> 1 [label=\"Saved\", color=darkgreen, fontcolor=darkgreen]\n        3 -> 1 [label=\"Purchased\", color=darkgreen, fontcolor=darkgreen]\n        4 -> 1 [label=\"Complete\", color=darkgreen, fontcolor=darkgreen]\n\n        2 -> 4 [label=\"Failure\", color=red, fontcolor=red]\n        3 -> 4 [label=\"Failure\", color=red, fontcolor=red]\n    }\n\n    subgraph cluster1 \n    {\n        label=User\n        color=slateblue2\n        fontcolor=slateblue4\n        margin=15\n        labeljust=l\n\n        userStart [label="", style=invis]\n        userStart -> 7 [label=\"Start\", weight=2, color=darkgreen, fontcolor=darkgreen]\n\n        0 -> 7 [xlabel=\"Start\", lhead=cluster1, color=darkgreen, fontcolor=darkgreen]\n\n        6 [label=\"FullAccess\", color=coral3, fontcolor=coral4]\n        7 [label=\"Trial(count: Int)\", color=coral3, fontcolor=coral4]\n        7 -> 6 [label=\"Purchased\", color=darkgreen, fontcolor=darkgreen]\n    }\n    \n    7 -> 1 [xlabel=\"Save\", color=blue, fontcolor=blue3, lhead=cluster0, ltail=cluster1, weight=0.9, minlen=7]\n    2 -> 7 [xlabel=\"Saved\", color=darkgreen, fontcolor=darkgreen, lhead=cluster1, ltail=cluster0, weight=0.9, minlen=7]\n    3 -> 7 [xlabel=\"Purchased\", color=darkgreen, fontcolor=darkgreen, lhead=cluster1, ltail=cluster0, weight=0.9, minlen=7]\n}"
-//applicationSchema == applicationSchemaString
-let inputFinal = applicationSchema
-let ouputFinal = parse(graph, applicationSchema).result
-
-
-//: # Imports
 
 
 
--- SwiftReferenceApp/RxSelectMany.playground/Contents.swift
+++ SwiftReferenceApp/RxSelectMany.playground/Contents.swift
@@ -3,11 +3,10 @@
 //  Copyright (c) 2015 StoryShare. All rights reserved.
 
 import RxSwift
-import RxBlocking
+//import RxBlocking
 import XCPlayground
 
-//XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
-
+XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
 
 infix operator >+ { associativity left precedence 90 }
 
@@ -23,13 +22,13 @@ func toArray<T>(observable: Observable<T>) -> [T] {
 } 
 
 
-func toArray2<E>(observable: Observable<E>) -> [E] {
-    return (observable 
-    >- reduce( [E]() ) { (var arr, e) in
-        arr.append(e)
-        return arr
-    } >- last).get()!
-} 
+//func toArray2<E>(observable: Observable<E>) -> [E] {
+//    return (observable 
+//    >- reduce( [E]() ) { (var arr, e) in
+//        arr.append(e)
+//        return arr
+//    } >- last).get()!
+//} 
 
 var count = 0
 func example<T>(of message: String, closure: () -> T) -> T  {
@@ -45,76 +44,160 @@ func example<T>(of message: String, closure: () -> T) -> T  {
     return result 
 }
 
-let seq = example(of: "Generating a sequence `seq` using a range.") 
-{
-   return from(1...3)  
-}
+//let seq = example(of: "Generating a sequence `seq` using a range.") 
+//{
+//   return from(1...3)  
+//}
+////
+//example(of: "Subscribe and calculate (x, x²)") 
+//{
+//    seq >+ { x in 
+//        let y = x * x 
+//        println("\t(x, y) = \(x, y)") }
+//}
+//    
+//let seqOfSeq = example(of: "Generating a sequence of sequences.") 
+//{
+//    seq >- map 
+//                { i -> Observable<Int> in  
+//    if i <= 3  { return from(1...i) }
+//            else { return empty() }
+//    }
+//}
+//
+//example (of: "Unfolding the sequences, commas wrong here. • How can we fix it?") { _ -> Void in
+//    print("\n\tseqOfSeq = ")
+//    seqOfSeq >+  { seq in
+//        print("{")
+//        seq >+  { x in 
+//            x; print("\(x),")
+//            }
+//        print("}")
+//        }
+//    println()
+//}
+//
+//
+//let flatMapped : Observable<Int> = 
+//    example(of: "flatMap using take(1) and skip(1)") 
+//    { 
+//        let  xs = seqOfSeq >- flatMap {$0 } 
+//        print("\t[")
+//        xs >- take(1) >+ { x in print(x) }
+//        xs >- skip(1) >+  { x in print(", \(x)") } 
+//        println("]")
+//        let a = Array(arrayLiteral: xs)
+//        return xs
+//    }
+//
+//
+//let concatSeq = example(of: "concatenation of a `seq` of `seq`") { 
+////    let 
+//    return seqOfSeq >- concat
+////    println(s >- toArray)
+////   return s
+//}
+//
+// 
+//func timedConcat() 
+//{
+//    let timedSeq =
+//        interval(1.0, MainScheduler.sharedInstance)
+//        >- take(6)
+//        >- map { a in 
+//            return a
+//            }
+//    timedSeq >- subscribeNext { x in
+//        x
+//        println(x)
+//    }
+//}
+//
+////timedConcat()
+//
 
-example(of: "Subscribe and calculate (x, x²)") 
-{
-    seq >+ { x in 
-        let y = x * x 
-        println("\t(x, y) = \(x, y)") }
-}
+import RxSwift
+
+let xs1 = from(1...3)
+let xs2 = from(1...2)
+//
+////: `merge` without temporal effects
+//example(of: "merge") { Void -> Void in 
+//    
+//    let xs = from([xs1, xs2])
+//    let merged = merge(xs) //>- variable
+//    
+//    merged >- subscribeNext 
+//        { println($0) }
+//    
+//    let sub = merged >- 
+//        subscribeCompleted { 
+//            println("`merged` Completed") }
+//}
+////: `concat` without temporal effects.
+//example(of: "concat") { Void -> Void in
+//    
+//    let concatenated = concat([xs1, xs2]) //>- variable
+//    concatenated >- subscribeNext 
+//        { println($0) }
+//    
+//    concatenated >- 
+//        subscribeCompleted { 
+//            println("`concatenated` Completed") }
+//        
+//}
+
+//: `merge` with temporal effects.
+example(of: "timed merge") { () -> Void in
+    let ts1 =
+    interval(0.1, MainScheduler.sharedInstance)
+        >- take(3)
+    //    t1s >- subscribeNext { println($0) }
+    let ts2 =
+    interval(0.2, MainScheduler.sharedInstance)
+        >- take(3)
+        
+    let ts = from([ts1, ts2])
     
-let seqOfSeq = example(of: "Generating a sequence of sequences.") 
-{
-    seq >- map 
-                { i -> Observable<Int> in  
-    if i <= 3  { return from(1...i) }
-            else { return empty() }
-    }
+    let merged = merge(ts) //>- variable
+    merged >- subscribeNext 
+        { println($0) }
+    
+    merged >- 
+        subscribeCompleted { 
+            println("`merged` Completed") }
 }
 
-example (of: "Unfolding the sequences, commas wrong here. • How can we fix it?") { _ -> Void in
-    print("\n\tseqOfSeq = ")
-    seqOfSeq >+  { seq in
-        print("{")
-        seq >+  { x in 
-            x; print("\(x),")
-            }
-        print("}")
-        }
-    println()
-}
-
-
-let flatMapped : Observable<Int> = 
-    example(of: "flatMap using take(1) and skip(1)") 
-    { 
-        let  xs = seqOfSeq >- flatMap {$0 } 
-        print("\t[")
-        xs >- take(1) >+ { x in print(x) }
-        xs >- skip(1) >+  { x in print(", \(x)") } 
-        println("]")
-        let a = Array(arrayLiteral: xs)
-        return xs
-    }
-
-
-let concatSeq = example(of: "concatenation of a `seq` of `seq`") { 
-//    let 
-    return seqOfSeq >- concat
-//    println(s >- toArray)
-//   return s
-}
-
- 
-func timedConcat() 
-{
-    let timedSeq =
-        interval(1.0, MainScheduler.sharedInstance)
-        >- take(6)
-        >- map { a in 
-            return a
-            }
-    timedSeq >- subscribeNext { x in
-        x
-        println(x)
-    }
-}
-
-//timedConcat()
+////: `concat` with temporal effects.
+//example(of: "timed concat") { () -> Void in
+////
+//    let ts1 = interval(0.2, MainScheduler.sharedInstance)
+//        >- take(3)
+//        
+//    let ts2 = interval(0.2, MainScheduler.sharedInstance)
+//        >- take(3)
+//        
+//    let ts = [ts1, ts2]
+//    
+//    let concatenated = concat(ts) 
+//    concatenated >- subscribeNext { println($0) }
+//    
+//    concatenated >- subscribeCompleted { println("`concatenated` Completed") }
+//    
+//   
+//}
+//{
+//    let timedSeq =
+//        interval(1.0, MainScheduler.sharedInstance)
+//        >- take(6)
+//        >- map { a in 
+//            return a
+//            }
+//    timedSeq >- subscribeNext { x in
+//        x
+//        println(x)
+//    }
+//}
 
 
 
